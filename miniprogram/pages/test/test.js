735822cc727bf14ebb70eda60490ab29726a3668
kufei88 <117672825@qq.com>
2018-09-13 23:13:56 +0800

    first commit

---
+++ miniprogram/pages/test/test.js
@@ -0,0 +1,18 @@
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+  },
+  onLoad: function () {
+    console.log('onLoad test');
+  }
+}) 
